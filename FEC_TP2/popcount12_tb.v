4b9fee64562a06e35b1ba7c7c3152526d81f28d3
Angelica Paredes <ing.electronica.1617@gmail.com>
2026-09-09 21:22:34 -0300

    comentarios

--- FEC_TP2/popcount12_tb.v
+++ FEC_TP2/popcount12_tb.v
@@ -1,6 +1,6 @@
 module popcount12_tb;
-    reg [11:0] i_vec; //entrada que yo controlo en el testbench - VECTOR
-    wire [3:0] o_weight; // salida: 4 pines que representan el peso de Hamming del vector de entrada
+    reg [11:0] i_vec; //Entrada: 12 pines que representan el vector de bits de entrada
+    wire [3:0] o_weight; // Salida: 4 pines que representan el peso de Hamming del vector de entrada
 
     popcount12 poprcount_inst (
         .i_vec(i_vec),
@@ -10,10 +10,9 @@ module popcount12_tb;
     initial begin
         i_vec = 12'b100111001111; // Entrada de prueba
 
-        #10; // Esperar 10 unidades de tiempo para que se propague la señal y_weight
+        #10;
 
-        //Resultado
-        $display("\ni_vec=%b  \no_weight=%d\n", i_vec, o_weight);
-        $finish; //Terminar la simulacion
+        $display("\ni_vec=%b  \no_weight=%d (peso) \n", i_vec, o_weight);
+        $finish;
     end
 endmodule
